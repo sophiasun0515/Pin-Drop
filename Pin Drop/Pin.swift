925a9c1c94060c6d8e25d13ce70384da03dc47cc
sophiasun0515 <ruixuansun@gatech.edu>
2017-10-24 19:58:00 -0400

    first commit

---
+++ Pin Drop/Pin.swift
@@ -0,0 +1,13 @@
+//
+//  Pin.swift
+//  Pin Drop
+//
+//  Created by Apple on 10/24/17.
+//  Copyright © 2017 Sophia's Secret Garden. All rights reserved.
+//
+
+import UIKit
+
+class Pin: MKAnnotation {
+
+}
